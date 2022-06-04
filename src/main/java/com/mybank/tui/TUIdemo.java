54d75996a99c587f8d8296620caba5787239942d
vladavasileva <91936629+vladavasileva@users.noreply.github.com>
2022-06-04 21:29:39 +0300

    Add files via upload

---
+++ src/main/java/com/mybank/tui/TUIdemo.java
@@ -0,0 +1,101 @@
+package com.mybank.tui;
+
+import com.mybank.tui.data.DataSource;
+import com.mybank.tui.domain.*;
+import jexer.TAction;
+import jexer.TApplication;
+import jexer.TField;
+import jexer.TText;
+import jexer.TWindow;
+import jexer.event.TMenuEvent;
+import jexer.menu.TMenu;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Alexander 'Taurus' Babich
+ */
+public class TUIdemo extends TApplication {
+
+    private static final int ABOUT_APP = 2000;
+    private static final int CUST_INFO = 2010;
+
+    private static final DataSource dataSource = new DataSource("src\\main\\java\\com\\mybank\\tui\\data\\test.dat");
+
+    public static void main(String[] args) throws Exception {
+        TUIdemo tdemo = new TUIdemo();
+        (new Thread(tdemo)).start();
+    }
+
+    public TUIdemo() throws Exception {
+        super(BackendType.SWING);
+
+        addToolMenu();
+        //custom 'File' menu
+        TMenu fileMenu = addMenu("&File");
+        fileMenu.addItem(CUST_INFO, "&Customer Info");
+        fileMenu.addDefaultItem(TMenu.MID_SHELL);
+        fileMenu.addSeparator();
+        fileMenu.addDefaultItem(TMenu.MID_EXIT);
+        //end of 'File' menu
+
+        addWindowMenu();
+
+        //custom 'Help' menu
+        TMenu helpMenu = addMenu("&Help");
+        helpMenu.addItem(ABOUT_APP, "&About...");
+        //end of 'Help' menu
+
+        setFocusFollowsMouse(true);
+        //Customer window
+        ShowCustomerDetails();
+    }
+
+    @Override
+    protected boolean onMenu(TMenuEvent menu) {
+        if (menu.getId() == ABOUT_APP) {
+            messageBox("About", "\t\t\t\t\t   Just a simple Jexer demo.\n\nCopyright \u00A9 2019 Alexander \'Taurus\' Babich").show();
+            return true;
+        }
+        if (menu.getId() == CUST_INFO) {
+            ShowCustomerDetails();
+            return true;
+        }
+        return super.onMenu(menu);
+    }
+
+    private void ShowCustomerDetails() {
+        TWindow custWin = addWindow("Customer Window", 2, 1, 40, 10, TWindow.NOZOOMBOX);
+        custWin.newStatusBar("Enter valid customer number and press Show...");
+
+        custWin.addLabel("Enter customer number: ", 2, 2);
+        TField custNo = custWin.addField(24, 2, 3, false);
+        TText details = custWin.addText("Owner Name: \nAccount Type: \nAccount Balance: ", 2, 4, 38, 8);
+        custWin.addButton("&Show", 28, 2, new TAction() {
+            @Override
+            public void DO() {
+                try {
+                    dataSource.loadData();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                try {
+                    int customerNum = Integer.parseInt(custNo.getText());
+                    Customer customer = Bank.getCustomer(customerNum);
+                    String accountType;
+                    if(customer.getAccount(0) instanceof SavingsAccount) {
+                        accountType = "saving";
+                    } else {
+                        accountType = "checking";
+                    }
+
+                    details.setText("Owner Name: " + customer.getFirstName() + " " + customer.getLastName() + "  (id=" + customerNum + ")\nAccount Type: '" + accountType + "'\nAccount Balance: " + customer.getAccount(0).getBalance());
+                } catch (Exception e) {
+                    messageBox("Error", "You must provide a valid customer number.").show();
+                }
+            }
+        });
+    }
+}
